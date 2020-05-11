474ae45c2452c2173ae9a4f28fcb9b67a9a152e8
KingSri <srirang97@gmail.com>
2020-05-10 19:38:33 -0700

    changing settings

--- sricalendar/settings.py
+++ sricalendar/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SRICALENDAR_KEY')
+SECRET_KEY = '=)e%#q$&jxy)1nul32b^r^=6m3vf3-01+tl_5+dp0&bfpmgj#s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
